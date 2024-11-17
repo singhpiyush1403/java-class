367b99f0a407f59360c937f2a1d59021f8a81966
PIYUSH SINGH <singhpiyush1403@gmail.com>
2024-11-18 01:32:36 +0530

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,12 @@
+  class Main{
+    int id;
+    String name;
+    public static void main(String args[]){
+        System.out.println("customer name :");
+        Main piyush = new Main();
+        piyush.id = 684;
+        piyush.name = "piyush kumar";
+        System.out.println(piyush.id);
+        System.out.println(piyush.name);
+    }
+}
